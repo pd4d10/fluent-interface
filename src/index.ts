9a2aaee51bc0801ddf76542ba80b9b6dc529afab
Rongjian Zhang <pd4d10@gmail.com>
2022-12-15 17:58:04 +0800

    feat: initial implementation

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,20 @@
-console.log("Hello World");
+export default function fi<
+  T extends Object,
+  M extends Record<string, (t: T, ...args: any[]) => any>
+>(t: T, methods: M) {
+  const proto: Record<string, any> = {};
+  Object.keys(methods).forEach((name) => {
+    proto[name] = function (...args: any[]) {
+      const next = methods[name](this as T, ...args);
+      Object.setPrototypeOf(next, proto);
+      return next;
+    };
+  });
+  Object.setPrototypeOf(t, proto);
 
-export default function () {}
+  return t as {
+    [P in keyof M]: M[P] extends (t: any, ...args: infer V) => infer R
+      ? (...args: V) => R
+      : never;
+  };
+}
